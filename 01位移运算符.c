ffa13cd27f4a643de29ba3e215344bc52bb00e28
Wu zhiyi <127090533+PLUSPLUSzy@users.noreply.github.com>
2023-03-20 23:36:51 +0800

    位移运算符的学习

---
+++ 01位移运算符.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main01()
+{
+	int a=10;
+	printf("%d\n",a<<2);//左移乘以2的n次幂
+	return 0;
+}
+
+int main()
+{
+	int b=20;
+	printf("%d\n",b>>2);//右移除以2的n次幂
+	return 0;
+}
