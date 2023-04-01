9b91fb3baad53458fe56f9b48f815567a2302a63
Wu zhiyi <127090533+PLUSPLUSzy@users.noreply.github.com>
2023-04-01 16:41:59 +0800

    scanf practise

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include<stdio.h>
+#include<string.h>
 
 int main() {
-    int i=123;
-    printf("%d\n",i);//十进制
-    printf("%o\n",i);//八进制
-    printf("%x\n",i);//十六进制
+    int i,ret;
+    char c;
+    double d;
+    ret=scanf("%d %c%lf",&i,&c,&d);//123a 3.14多种数据类型混合输入
+    printf("i=%d,c=%c,d=%lf",i,c,d);
+//    scanf("%d",&i);
+//    printf("i=%d\n",i);
+//    fflush(stdin);
+//    scanf("%c",&c);
+//    printf("c=%c\n",c);
     return 0;
 }
