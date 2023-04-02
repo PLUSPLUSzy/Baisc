d573eb1cafab44dc6e2199489f282b8e57b4ba96
Wu zhiyi <127090533+PLUSPLUSzy@users.noreply.github.com>
2023-04-02 22:27:39 +0800

    闰年计算

--- main.cpp
+++ main.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
-#include<stdio.h>
-#include<string.h>
 
 int main() {
-    int i,ret;
-    char c;
-    double d;
-    ret=scanf("%d %c%lf",&i,&c,&d);//123a 3.14多种数据类型混合输入
-    printf("i=%d,c=%c,d=%lf",i,c,d);
-//    scanf("%d",&i);
-//    printf("i=%d\n",i);
-//    fflush(stdin);
-//    scanf("%c",&c);
-//    printf("c=%c\n",c);
+    int year;
+    scanf("%d",&year);
+    if(year%4==0&&year%100!=0||year%400==0)
+    {
+        printf("yes\n");
+    }
+    else
+    {
+        printf("no\n");
+    }
     return 0;
 }
